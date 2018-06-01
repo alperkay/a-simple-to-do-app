328a6b78ec09ac165eec949b20624647f1a7391a
alper <alperkay@hotmail.com>
2018-06-01 09:55:41 +0200

    empty list still not working properly

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 //const todoItems= ['write user stories', 'break first feature down into steps', 'make first feature', 'make todo app']
-const todoItems= []
-let todoList = document.getElementById("todo_list")
-
-/*
-todoItems.forEach(function(item, index) {
-  var listItem = document.createElement("li");
-  var todoText = document.createTextNode(item);
-  listItem.appendChild(todoText);
-  todoList.appendChild(listItem);
-})
-console.log(todoList)
-*/
+let todoItems= []
+let todoList = document.getElementById("todo_list");
+let itemList = document.getElementById("itemList");
 
 function addUserInput() {
   //declare a function called addUserInput which is called when user clicks the button
@@ -18,7 +9,7 @@ function addUserInput() {
     //declares a variable x and assign it to the value that's inputted to the textarea
     todoItems.push(x);
     //adds x to the todoItems array
-    let listItem = document.createElement("li");
+    let listItem = document.createElement("li", {"id": "itemList"});
     //create a list element
     let todoText = document.createTextNode(x);
     //create a text node with the input gotten from user input
@@ -27,22 +18,16 @@ function addUserInput() {
     todoList.appendChild(listItem);
     //append the list item to the list
     document.getElementById("myTextArea").value = "";
-    //clears the text area
-    console.log(todoItems)
-    //console log to check ourselves
+    listItem.addEventListener('click', removeOnClick);
 }
 
-function DeleteUserInput() {
-  let deleteItem = window.prompt("which to do item would you like to delete?");
-  //asks user which item to delete from the list
-  if (deleteItem !== null && isNaN(deleteItem)== false && deleteItem<=todoItems.length)  {
-    //if statement to check if the user made an input which is a number and within the range of the array's length
-    let removedItem = todoItems.splice(deleteItem-1,1);
-    //remove the item from the array
-    todoList.removeChild(document.getElementsByTagName("li")[deleteItem-1]);
-    //remove the item from the page
-  } else { window.alert("hey, please input a valid number")}
-  //pops up an alert if the input is invalid
+function removeOnClick() {
+  const removedIndex = todoItems.findIndex(removed => removed === this.innerHTML);
+  delete todoItems[removedIndex];
+  this.style.textDecoration= "line-through";
   console.log(todoItems);
-  //console log to check ourselves
+}
+
+function DeleteAll() {
+  todoItems= [];
 }
